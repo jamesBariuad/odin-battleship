1b1393a9b1d37c83e2ca30777cedcdd34193a895
jokandro <114486039+jokandro@users.noreply.github.com>
2023-09-25 15:49:53 +0800

    feat: Gameboard Factory Function    can place ships, track attacks, check if gameover

--- battleship.test.js
+++ battleship.test.js
@@ -1,4 +1,4 @@
-import { Ships } from "./battleship";
+import { Ships, GameBoard } from "./battleship";
 
 test("ship hit", () => {
   const testShips = Ships();
@@ -9,6 +9,43 @@ test("ship hit", () => {
 test("ship sunk", () => {
   const testShips = Ships();
   testShips.patrolBoat.hit();
-  testShips.patrolBoat.isSunk()
+  testShips.patrolBoat.isSunk();
   expect(testShips.patrolBoat.stats.sunk).toBe(true);
+});
+
+test("placing a ship", () => {
+  const testBoard = GameBoard();
+  const testShips = Ships();
+  const battleship = testShips.battleship;
+  const submarine = testShips.submarine;
+  testBoard.placeShips(0, 0, battleship, "horizontal");
+  testBoard.placeShips(5, 5, submarine, "vertical");
+  expect(battleship.coordinates).toStrictEqual([
+    [0, 0],
+    [1, 0],
+    [2, 0],
+    [3, 0],
+  ]);
+  expect(submarine.coordinates).toStrictEqual([
+    [5, 5],
+    [5, 6],
+  ]);
+});
+
+test("receiveAttackHit", () => {
+  const testBoard = GameBoard();
+  const testShips = Ships();
+  const submarine = testShips.submarine;
+  testBoard.placeShips(5, 5, submarine, "vertical");
+  expect(testBoard.receiveAttack(5, 5, testShips)).toBe("Hit!");
+  expect(submarine.stats.timesHit).toBe(1);
+});
+
+test("receiveAttackMissed", () => {
+  const testBoard = GameBoard();
+  const testShips = Ships();
+  const submarine = testShips.submarine;
+  testBoard.placeShips(5, 5, submarine, "vertical");
+  expect(testBoard.receiveAttack(9, 9, testShips)).toBe("Miss!");
+  expect(testBoard.missedCoordinates).toStrictEqual([[9,9]])
 });
